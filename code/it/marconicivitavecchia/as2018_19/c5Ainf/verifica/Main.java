c35d9e27de75df5b7642b35c036c2348f3370dfd
Your Name <you@example.com>
2018-11-17 11:25:31 +0100

    consegna compito

--- code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
+++ code/it/marconicivitavecchia/as2018_19/c5Ainf/verifica/Main.java
@@ -1,7 +1,7 @@
 
 // TODO Inserire il package corretto
 // nota: potete usare il suggerimento della lampadina a sinistra (scegliete l'unica opzione sensata). 
-package __TODO__;
+package it.marconicivitavecchia.as2018_19.c5Ainf.verifica;
 
 import javax.swing.*;
 
@@ -12,16 +12,44 @@ public class Main extends JFrame {
 		super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		
 		// TODO Inserire la size in accordo con il wireframe (valori approssimati)
-		super.setSize(__TODO__,__TODO__);
+		super.setSize(400,600);
 		
 		// TODO Usare StringBuilder per costruire la stringa HTML
-		
+		StringBuilder sb = new StringBuilder("<html>");
 		// TODO Creare la stringa HTML per il wireframe
+		sb.append("<h1><b>Speed Test<b></h1>"); //Titolo
+		sb.append("<div style='table'>");
+		//Riga 1
+		sb.append("<divstyle='display:table-row'>");
+		
+		sb.append("<divstyle='display:table-cell'>");
+		sb.append("Date");
+		sb.append("Up(Mbps)");
+		sb.append("Down(Mbps)");
+		sb.append("</div>");
+		//Riga 2
+		sb.append("<divstyle='display:table-row'>");
+				
+		sb.append("<divstyle='display:table-cell'>");
+		sb.append("1/7/18");
+		sb.append("25.0");
+		sb.append("28.9");
+		sb.append("</div>");
+		//Riga 3
+		sb.append("<divstyle='display:table-row'>");
+				
+		sb.append("<divstyle='display:table-cell'>");
+		sb.append("7/9/18");
+		sb.append("24.6");
+		sb.append("1.99");
+		sb.append("</div>");
+		
 		
 		// TODO Convertire lo StringBuilder in String
-		
+		String stringa= sb.toString();
 		// TODO Assegnare correttamente la stringa create a JLabel
-		super.add(new JLabel(__TODO__));
+	
+		super.add(new JLabel(stringa));
 		super.setVisible(true);
 		 
 		System.out.println("Verifica Swing-GitHub");
